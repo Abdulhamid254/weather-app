1a8ee896923572584f41e85181c641ba922baac7
Abdulhamid254 <nassurabdulhamid49@gmail.com>
2023-11-02 05:32:58 -0700

    Implemented Apollo-client

--- apollo-client.ts
+++ apollo-client.ts
@@ -1,18 +1,14 @@
-// apollo client allows us to create a connection to our backend
-
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
-let client: ApolloClient<any> | null = null;
 
-export const getClient  = () => {
-    if (!client) {
-        client = new ApolloClient({
-            uri: process.env.API_URL,
-            cache: new InMemoryCache(),
-            headers: {
-                Authorization: `apikey ${process.env.NEXT_PUBLIC_STEPZEN_API_KEY}`,
-            }
-        });
-    }
+export const getClient = () => {
+    const client = new ApolloClient({
+        uri: process.env.API_URL,
+        cache: new InMemoryCache(),
+        headers: {
+            Authorization: `apikey ${process.env.NEXT_PUBLIC_STEPZEN_API_KEY}`,
+        },
+    });
     return client;
+
 }
--- typings.d.ts
+++ typings.d.ts
@@ -1,16 +1,31 @@
-interface CurrentWeather {
+interface Current {
+    cloudcover: number;
+    interval: number;
     is_day: number;
-    temperature: number;
+    relativehumidity_2m: number;
+    temperature_2m: number;
     time: string;
-    weatherCode: number;
-    winddirection: number;
-    windspeed: number;
-    
-}
-
-interface Daily {
+    weathercode: number;
+    winddirection_10m: number;
+    windspeed_10m: number;
+  }
+  
+  interface CurrentUnits {
+    cloudcover: string;
+    interval: string;
+    is_day: string;
+    relativehumidity_2m: string;
+    temperature_2m: string;
+    time: string;
+    weathercode: string;
+    winddirection_10m: string;
+    windspeed_10m: string;
+  }
+  
+  interface Daily {
     apparent_temperature_max: [number];
     apparent_temperature_min: [number];
+    precipitation_probability_max: [number];
     sunrise: [string];
     sunset: [string];
     temperature_2m_max: [number];
@@ -18,12 +33,13 @@ interface Daily {
     time: [string];
     uv_index_clear_sky_max: [number];
     uv_index_max: [number];
-    weather: [number]
-}
-
-interface DailyUnits {
+    weathercode: [number];
+  }
+  
+  interface DailyUnits {
     apparent_temperature_max: string;
-    apparent_temperature_min:string;
+    apparent_temperature_min: string;
+    precipitation_probability_max: string;
     sunrise: string;
     sunset: string;
     temperature_2m_max: string;
@@ -31,54 +47,53 @@ interface DailyUnits {
     time: string;
     uv_index_clear_sky_max: string;
     uv_index_max: string;
-    weatheRCODE: string;
-
-}
-
-interface Hourly {
-    apparent_temperature: [number]
-    precipitation: [number]
-    precipitation_probability: [number]
-    rain: [number]
-    relativehumidity_2m: [number]
-    showers: [number]
-    snow_depth: [number]
-    snowfall: [number]
-    temperature_2m: [number]
-    time: [string]
-    uv_index: [number]
-    uv_index_clear_sky: [number]
-    windgusts_10m: [number]
-}
-
-interface HourlyUnits {
-    apparent_temperature: string
-    precipitation: string
-    precipitation_probability: string
-    rain: string
-    relativehumidity_2m: string
-    showers: string
-    snow_depth: string
-    snowfall: string
-    temperature_2m: string
-    time: string
-    uv_index: string
-    uv_index_clear_sky: string
-    windgusts_10m: string
-}
-
-interface Root {
-    current_weather: CurrentWeather
-    current_weather_units: CurrentWeatherUnits
-    daily: Daily
-    daily_units: DailyUnits
-    elevation: Int
-    generationtime_ms: Float
-    hourly: Hourly
-    hourly_units: HourlyUnits
-    latitude: Float
-    longitude: Float
-    timezone: String
-    timezone_abbreviation: String
-    utc_offset_seconds: Int
+    weathercode: string;
+  }
+  
+  interface Hourly {
+    apparent_temperature: [number];
+    precipitation: [number];
+    precipitation_probability: [number];
+    rain: [number];
+    relativehumidity_2m: [number];
+    showers: [number];
+    snow_depth: [number];
+    snowfall: [number];
+    temperature_2m: [number];
+    time: [string];
+    uv_index: [number];
+    uv_index_clear_sky: [number];
+    windgusts_10m: [number];
+  }
+  
+  interface HourlyUnits {
+    apparent_temperature: string;
+    precipitation: string;
+    precipitation_probability: string;
+    rain: string;
+    relativehumidity_2m: string;
+    showers: string;
+    snow_depth: string;
+    snowfall: string;
+    temperature_2m: string;
+    time: string;
+    uv_index: string;
+    uv_index_clear_sky: string;
+    windgusts_10m: string;
+  }
+  
+  interface Root {
+    current: Current;
+    current_units: CurrentUnits;
+    daily: Daily;
+    daily_units: DailyUnits;
+    elevation: number;
+    generationtime_ms: number;
+    hourly: Hourly;
+    hourly_units: HourlyUnits;
+    latitude: number;
+    longitude: number;
+    timezone: string;
+    timezone_abbreviation: string;
+    utc_offset_seconds: number;
   }
