be55ed2297be97e6e8d72c02fcc7b82b5cdce82e
Abdulhamid254 <nassurabdulhamid49@gmail.com>
2023-11-02 12:07:04 -0700

    Fetching weatger queries from Graphql

--- app/location/[city]/[lat]/[long]/page.tsx
+++ app/location/[city]/[lat]/[long]/page.tsx
@@ -1,3 +1,6 @@
+import { getClient } from "@/apollo-client";
+import { fetchWeatherQueries } from "@/graphql/queries/fetchWeatherQueries";
+
 
 
 type Props = {
@@ -8,7 +11,25 @@ type Props = {
 }
 }
 
-const WeatherPage = ({params:{city,lat,long}}: Props) => {
+async function WeatherPage({params : {city , lat , long}}:Props) {
+  const client = getClient();
+
+  const {data} = await client.query({
+    query: fetchWeatherQueries,
+    variables:{
+      current_weather: "true",
+      latitude : lat,
+      longitude : long,
+      timezone : "auto",
+    }
+  })
+
+  const results : Root = data.myQuery;
+
+
+console.log(results);
+
+
   return (
     <div>Welcome to WeatherPage: {city} {lat} {long}</div>
   )
