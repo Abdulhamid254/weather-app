87e80d1ad9c45bbdb67eef5db7627e9a7b13dc68
Abdulhamid254 <nassurabdulhamid49@gmail.com>
2023-11-03 01:01:53 -0700

    Implemented the statcards in the weather page

--- app/location/[city]/[lat]/[long]/page.tsx
+++ app/location/[city]/[lat]/[long]/page.tsx
@@ -1,4 +1,6 @@
 import { getClient } from "@/apollo-client";
+import CalloutCard from "@/components/CalloutCard";
+import StatCard from "@/components/StatCard";
 import { fetchWeatherQueries } from "@/graphql/queries/fetchWeatherQueries";
 
 
@@ -31,7 +33,80 @@ console.log(results);
 
 
   return (
-    <div>Welcome to WeatherPage: {city} {lat} {long}</div>
+    <div>
+      {/* <InformationPanel/> */}
+
+      <div>
+        <div className="p-5">
+          <div className="pb-5">
+               <h2 className="text-xl font-bold">Todays Weather Overeview</h2>
+               <p className="text-sm text-gray-400">
+                Last Updated at:{""}
+                {new Date(results.current_weather.time).toLocaleString()}(
+                  {results.timezone}
+                )
+               </p>
+          </div>
+
+          <div className="m-2 mb-10">
+            <CalloutCard  message="This is where GPT-4 summary message will go!"/>
+          </div>
+
+          <div className="grid grid-cols-1 xl:grid-cols-2 gap-5 m-2">
+            <StatCard
+             title="Maximum Temperature"
+             metric={`${results.daily.temperature_2m_max[0].toFixed(1)}°`}
+             color="yellow"
+            
+            />
+          
+          <StatCard
+             title="Minimum Temperature"
+             metric={`${results.daily.temperature_2m_min[0].toFixed(1)}°`}
+             color="green"
+            
+            />
+
+            <div>
+            <StatCard
+             title="UV Index"
+             metric={`${results.daily.uv_index_max[0].toFixed(1)}`}
+             color="rose"
+            />
+            {Number(results.daily.uv_index_max[0].toFixed(1)) > 5 && (
+              <CalloutCard
+              message={"The UV is high today, be sure to wear SPF!"}
+              warning
+              />
+            )}
+            </div>
+
+            <div className="flex space-x-3">
+              <StatCard
+              title="Wind Speed"
+              metric={`${results.current_weather.windspeed.toFixed(1)}m/s`}
+              color="cyan"
+              />
+
+             <StatCard
+              title="Wind Direction"
+              metric={`${results.current_weather.winddirection.toFixed(1)}°`}
+              color="violet"
+              />
+            </div>
+          </div>
+        </div>
+
+        <hr className="mb-5" />
+
+
+        <div className="space-y-3">
+          {/* Tempchart
+          RainChart
+          HumidityChart */}
+        </div>
+      </div>
+    </div>
   )
 }
 
